6df7f496638dc82a8c8a5966553c4e7c9d552733
Tarun <tarun.heda@sevenlakes.com>
2021-01-21 15:36:18 -0800

    created project and added files for home page

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HomePage from './homepage.component'
+import './homepage.styles.scss'
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
