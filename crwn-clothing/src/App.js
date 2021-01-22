08374ad6d93368fa06fe88b1ec009beffd1a568a
Tarun <tarun.heda@sevenlakes.com>
2021-01-21 17:31:08 -0800

    broken home page into smaller components and applied styling

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import HomePage from './homepage.component'
-import './homepage.styles.scss'
+import HomePage from './pages/homepage/homepage.component'
+import './pages/homepage/homepage.styles.scss'
 
 function App() {
   return (
---
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './menu-item.component.scss'
+
+const MenuItem = ({ title, imageUrl, size }) => {
+    return(
+        <div className={`${size} menu-item`}>
+            <div className='background-image'
+                 style={{ backgroundImage: `url(${imageUrl})` }}>
+            </div>
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='sub-title'>SHOP NOW</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
