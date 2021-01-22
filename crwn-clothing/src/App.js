16f6b9eb0811063a03d85241807bc78dcd2a6151
Tarun <tarun.heda@sevenlakes.com>
2021-01-22 00:43:31 -0800

    adding shop page with collection preview component and collection item component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 import './pages/homepage/homepage.styles.scss'
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
