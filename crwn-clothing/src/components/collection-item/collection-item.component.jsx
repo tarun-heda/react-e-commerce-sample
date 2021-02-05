aebe2f2d8db635f7f16f75b16d64f44f03fcd5e2
Tarun <tarun.heda@sevenlakes.com>
2021-02-04 22:51:27 -0800

    implemented add to cart functionality with grouping in cart reducer

--- crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import './collection-item.styles.scss'
+import { connect } from 'react-redux';
+import CustomButton from '../custom-button/custom-button.component';
+import { addCartItem } from '../../redux/cart/cart.action';
+import './collection-item.styles.scss';
 
-const CollectionItem = ({ name, price, imageUrl }) => {
+const CollectionItem = ({ item, addCartItem }) => {
+    const { name, price, imageUrl } = item;
     return (
         <div className='collection-item'>
             <div className='image'
                 style={{ backgroundImage : `url(${imageUrl})`}}>
             </div>
+            <CustomButton inverted onClick={() => (addCartItem(item))}>ADD TO CART</CustomButton>
             <div className='collection-footer'>
                 <span className='name'>{ name }</span>
                 <span className='price'>{ `$${price}` }</span>
@@ -15,4 +20,8 @@ const CollectionItem = ({ name, price, imageUrl }) => {
     )
 }
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addCartItem: cartItem => dispatch(addCartItem(cartItem))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
